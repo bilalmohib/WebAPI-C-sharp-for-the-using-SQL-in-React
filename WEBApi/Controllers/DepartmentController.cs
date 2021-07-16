4e576691163be33a939792c02c2ba1e0b66815bf
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-07-16 17:50:42 +0500

    feat

--- WEBApi/Controllers/DepartmentController.cs
+++ WEBApi/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Data;
+using WEBApi.Models;
 
 namespace WEBApi.Controllers
 {
@@ -20,7 +21,7 @@ namespace WEBApi.Controllers
         {
             _configuration = configuration;
         }
-        //API Method to get all the department details
+        //GET API Method to get all the department details
         [HttpGet]
         public JsonResult Get()
         {
@@ -32,6 +33,33 @@ namespace WEBApi.Controllers
             {
                 myCon.Open();
                 using (SqlCommand mysqlCommand = new SqlCommand(query,myCon))
+                {
+                    myReader = mysqlCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    //Closing the reader
+                    myReader.Close();
+                    //Closing the Connection
+                    myCon.Close();
+                }
+            }
+            //Now Returning the data table as a Result
+            return new JsonResult(table);
+        }
+
+        //POST Method to Insert the data into the sql database
+        [HttpPost]
+        public JsonResult Post(Department department)
+        {
+            string query = @"INSERT INTO Department VALUES 
+            ('"+department.DepartmentName;
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand mysqlCommand = new SqlCommand(query, myCon))
                 {
                     myReader = mysqlCommand.ExecuteReader();
                     table.Load(myReader);
