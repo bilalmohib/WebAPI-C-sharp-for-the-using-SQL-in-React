662e9013439af7594d06ee57e2c209b669d95945
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-08-06 22:41:43 +0500

    OK TicketController is done

--- WEBApi/Controllers/TicketController.cs
+++ WEBApi/Controllers/TicketController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using WEBApi.Models;
 
 namespace WEBApi.Controllers
 {
@@ -11,5 +15,125 @@ namespace WEBApi.Controllers
     [ApiController]
     public class TicketController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public TicketController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        //GET API Method to get all the department details
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from Ticket";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BTMSAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand mysqlCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = mysqlCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    //Closing the reader
+                    myReader.Close();
+                    //Closing the Connection
+                    myCon.Close();
+                }
+            }
+            //Now Returning the data table as a Result
+            return new JsonResult(table);
+        }
+
+        //POST Method to Insert the data into the sql database
+        [HttpPost]
+        public JsonResult Post(Ticket ticket)
+        {
+            string query = @"INSERT INTO Ticket(TicketNo,SeatNo,Price) VALUES 
+            (
+               '" + ticket.TicketNo + @"',
+               '" + ticket.SeatNo + @"',
+               '" + ticket.Price + @"'
+            )
+            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BTMSAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand mysqlCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = mysqlCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    //Closing the reader
+                    myReader.Close();
+                    //Closing the Connection
+                    myCon.Close();
+                }
+            }
+            //Now Returning the response when the data is returned that is a message "Added Successfully"
+            return new JsonResult("Added Successfully");
+        }
+
+
+        //PUT Method to Update the data into the sql database table
+        [HttpPut]
+        public JsonResult Put(Ticket ticket)
+        {
+            string query = @"
+                UPDATE TICKET SET TicketNo = '" + ticket.TicketNo + "Seat_No = " + ticket.SeatNo + "Price = " + ticket.Price +
+                @"'
+                WHERE TicketNo = " + ticket.TicketNo + @"";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BTMSAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand mysqlCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = mysqlCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    //Closing the reader
+                    myReader.Close();
+                    //Closing the Connection
+                    myCon.Close();
+                }
+            }
+            //Now Returning the response when the data is returned that is a message "Added Successfully"
+            return new JsonResult("Updated Successfully");
+        }
+
+        //Delete Method to Delete the data into the sql database table
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                DELETE FROM TICKET WHERE TicketNo = '" + id + @"'";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("BTMSAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand mysqlCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = mysqlCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    //Closing the reader
+                    myReader.Close();
+                    //Closing the Connection
+                    myCon.Close();
+                }
+            }
+            //Now Returning the response when the data is returned that is a message "Added Successfully"
+            return new JsonResult("Deleted Successfully");
+        }
     }
 }
